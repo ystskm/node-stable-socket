26a5b4cd62de2db0f7c1fd713e494b16f566631d
ystskm <brilliantpenguin@gmail.com>
2016-05-10 23:55:02 +0900

    fix for the duplicated connect, v0.5.2

--- StableSocket.js
+++ StableSocket.js
@@ -18,7 +18,12 @@
   !has_mod || (module.exports = StableSocket);
 
   // extra exports
-  var DNS = require(__dirname + '/lib/dns.js');
+  var DNS;
+  if(typeof require == 'undefined') {
+    DNS = global.DNS || Function();
+  } else {
+    DNS = require(__dirname + '/lib/dns.js');
+  }
   StableSocket.DNS = DNS;
 
   /**
@@ -57,18 +62,21 @@
 
   };
 
+  // Sending Data
   var Converter = function(rid, obj) {
     return [{
       rid: rid
     }, obj];
   };
 
+  // Receiving Data
   var Analyzer = function(msg) {
     return JSON.parse(msg);
   };
 
   var browsing = typeof process == 'undefined';
   var _rid = 0, _timers = {}, _callbacks = {}, _connector = {};
+  var k, stdout;
 
   // necessary for DNS lookup
   var Sockets = [];
@@ -109,29 +117,37 @@
     ss._silent_timer = null;
 
     // logger with care for non-enough object
-    ss.logger = opts.logger ? opts.logger: console;
+    ss.logger = opts.logger ? opts.logger: Function();
     ['log', 'error'].forEach(function(k) {
-      if(typeof ss.logger[k] == 'function') {
-        return;
-      }
-      ss.logger[k] = function() {
-        console.log.apply(console, arguments);
-      };
+
+      isFunction(ss.logger[k]) || (ss.logger[k] = function() {
+        if(ss.stdout() === false) return;
+        global.console.log.apply(global.console, arguments);
+      });
+
     });
 
+    // Initialize EventListeners
+    var evts = ['open', 'message', 'error', 'close', 'denied'];
     ss._index = 0, ss._conn = null, ss._waits = [];
-    ['open', 'mesage', 'error', 'close', 'denied'].forEach(function(evt_n) {
+
+    // Stable Socket is NOT a event emitter.
+    // Only one function can efficient for each object. ( onopen, ... )
+    evts.forEach(function(evt_n) {
       ss['on' + evt_n] = Function();
     });
 
     // kick lookup checker if not exists
+    if(!DNS.lookup) {
+      return;
+    }
     if(opts.lookup_check !== false && IntervalTimer == null) {
       startDNSInterval(opts);
     }
 
   }
 
-  var SSProtos = {
+  var proto = {
 
     connect: connect,
     isConnecting: isConnecting,
@@ -141,13 +157,24 @@
 
     send: send,
     close: close,
+
+    // addListeners: addListeners,
+    removeListeners: removeListeners,
+
     toSilentMode: toSilentMode,
     toActiveMode: toActiveMode,
-    redefineCandidates: redefineCandidates
+    redefineCandidates: redefineCandidates,
+    startDNSInterval: startDNSInterval,
+
+    stdout: function(v) {
+      v == null || (stdout = v);
+      return stdout;
+    }
 
   };
-  for( var i in SSProtos)
-    StableSocket.prototype[i] = SSProtos[i];
+  for(k in proto) {
+    StableSocket.prototype[k] = proto[k];
+  }
 
   /**
    * @prototype
@@ -172,7 +199,7 @@
     if(_connector[ConnectURI] != null) {
       // reconnecting warning
       logger.log('[StableSocket] ' + new Date().toGMTString() + ' - ');
-      logger.log('Overwrite connector before close for: ' + ConnectURI);
+      logger.log('  Overwrite connector before close for: ' + ConnectURI);
       onClose();
     }
 
@@ -183,25 +210,33 @@
     ss._host = ConnectURI.split('/').slice(0, 3).join('/');
     ss._conn = true; // on connecting sign
 
-    so.onopen = onOpen;
-    so.onmessage = onMessage;
-
-    so.onerror = function(e) {
-      onOpeningError(e), so.close(); // close to stop reconnecting
-    };
-    so.onclose = function(e) {
-      ss._open_error || onClose(e);
+    var evts_map = so.EventHandler = {
+      open: onOpen,
+      data: onMessage,
+      message: onMessage,
+      close: onClose
     };
 
-    function onOpen() {
+    if(isFunction(so.addEventListener)) {
+      Object.keys(evts_map).forEach(function(evt_ty) {
+        so.addEventListener(evt_ty, evts_map[evt_ty]);
+      });
+    } else {
+      Object.keys(evts_map).forEach(function(evt_ty) {
+        so['on' + evt_ty] = evts_map[evt_ty];
+      });
+    }
+
+    function onOpen(evt) {
 
       ss.onLine = true, ss._times['LastOpen'] = Date.now();
       if(ss._silent_timer) {
         return;
       }
 
-      // off opening error.
+      // Off opening event handler and opening error handler.
       onOpeningError = Function();
+      ss.removeListeners(so, ['open']);
 
       // when open socket, assign as his own socket.
       // (by readyState judge, occasionally not better.)
@@ -216,8 +251,7 @@
         msg = 'StableSocket Connection is ALREADY OPEN. \n';
         msg += 'Use another socket readyState:' + rs;
         msg += ', silently close the open socket. \n';
-        so.onmessage = so.onerror = so.onclose = Function();
-        so.close();
+        ss.close();
 
       }
 
@@ -228,7 +262,7 @@
       ss._waits = [];
 
       // re-send the waiting requests
-      while(waits.length) {
+      while (waits.length) {
         ss.send.apply(ss, waits.shift());
       }
 
@@ -237,10 +271,11 @@
 
     }
 
-    function onOpeningError(e) {
+    function onOpeningError(e, keep) {
 
-      // off opening error.
+      // Off opening error.
       onOpeningError = Function();
+      ss.removeListeners(so);
 
       try {
         // Destroy the creating socket 
@@ -272,15 +307,10 @@
         logger.log('retryConnect remains: ', ss._open_retry, ss._open_retrya);
 
         var intv = ss._open_retrya[0];
-        if(typeof intv != 'number') {
-          return false;
-        }
-
+        if(!is('number', intv)) return false;
         setTimeout(function() {
 
-          if(ss.isConnecting()) {
-            ss._conn = null;
-          }
+          if(ss.isConnecting()) ss._conn = null;
           ss.connect(rid);
 
         }, ss._open_retrya[0]);
@@ -315,9 +345,10 @@
       logger.log('Goto removing ' + waits.length + ' requests.');
 
       try {
-        while(waits.length) {
-          wait = waits.shift(), cb = wait.pop();
-          typeof cb != 'function' || cb.requestError(e, false);
+        while (waits.length) {
+          wait = waits.shift();
+          cb = wait.pop();
+          !isFunction(cb) || cb.requestError(e, false);
         }
       } catch(e) {
 
@@ -328,7 +359,7 @@
 
     }
 
-    function onMessage(m) {
+    function onMessage(evt) {
 
       if(so !== ss._conn) {
         msg = 'StableSocket detects not-primary socket message.';
@@ -347,33 +378,58 @@
 
         // Data analyzed by analyzer.
         // "WebSocket" => raw message, "EventSource" => wrapped event object
-        var raw = ss._host.indexOf('ws') == 0 ? m: m.data;
+        var raw = ss._host.indexOf('ws') == 0 ? evt: evt.data;
         var data = (opts.analyzer || Analyzer)(raw) || '';
 
         // and callback if exist.
         var h = data[0] || '', b = data[1] || '', rid = h.rid;
-        (_callbacks[rid] || Function())(b), _reset(rid);
+        var cb = _callbacks[rid];
+
+        // callback with 1st argument treat as "SUCCESSFULLY" 
+        // for the function(data, callback){ ... } type.
+        !isFunction(cb) || cb(b);
+        _reset(rid);
 
         // Get raw message.
-        ss.onmessage(m, data);
+        ss.onmessage(evt, data);
 
       } catch(e) {
 
-        // Get raw message. (unparsable message)
-        ss.onmessage(m, false);
+        // Get raw message. (exparsable message)
+        ss.onmessage(evt, false);
 
       }
     }
 
-    function onClose() {
+    function onClose(evt) {
 
-      if(so !== ss._conn) {
-        msg = 'StableSocket detects not-primary socket close.';
+      if(ss._open_error) return;
+
+      // Should always remove all listeners.
+      ss.removeListeners(so);
+      msg = '[StableSocket.onClose] ';
+
+      var pre_co = ss._conn;
+      if(so !== pre_co) {
+        switch(pre_co) {
+
+        case null:
+          msg += 'Detects reconnect.';
+          break;
+
+        case true:
+          msg += 'Detects connecting socket error.';
+          break;
+
+        default:
+          msg += 'Detects not-primary socket close.';
+
+        }
         return logger.log(msg);
       }
 
       ss.onLine = false, ss._times['LastClose'] = new Date();
-      msg = 'StableSocket Connection is CLOSED. ';
+      msg += 'Connection is CLOSED. ';
 
       var ConnectURI = (conf || '').ConnectURI;
       logger.log(msg + '(' + ConnectURI + ')');
@@ -384,9 +440,7 @@
       }
 
       var _so = _connector[ConnectURI];
-      if(_so == null) {
-        return;
-      }
+      if(_so == null) return;
 
       if(_so.readyState != Socket.CLOSED) {
         try {
@@ -435,20 +489,32 @@
     var mes, logger = ss.logger, opts = ss.options;
     var _waits = ss._waits, max_wait = opts.max_wait;
 
-    var args = Array.prototype.slice.call(arguments);
-    var callback = null, _cb = args[args.length - 1];
+    var args = casting(arguments);
+    var callback = null, options = {}, _cb = args[args.length - 1];
+
+    if(!isFunction(_cb)) {
+      args.push(_cb = Function());
+    }
+    if(args.length >= 3) {
+      options = args[args.length - 2] || {};
+    }
+
+    var _cbOpcd;
     if(opts.callback) {
+
       // If opts.callback available, set callback with RETRY parameter
       // (Default:true)
-      if(typeof _cb == 'function') {
 
-        // Set RETRY parameter for each request callback
-        callback = _cb.RETRY == null ? function() {
-          _cb.apply(this, arguments);
-        }: _cb;
-        args[args.length - 1] = callback;
+      // The "_cb" occasionally options
+      // "opcodeCallback" for the easy check
+      _cbOpcd = !!options.opcodeCallback;
 
-      }
+      // Set RETRY parameter for each request callback
+      callback = _cb.RETRY == null ? function() {
+        _cb.apply(this, arguments);
+      }: _cb;
+      args[args.length - 1] = callback;
+
     }
 
     // at the silent mode, "send" method immediately end.
@@ -459,23 +525,27 @@
 
     // request identifier
     var rid = ++_rid & 0xffffff;
-
     if(callback) {
-      callback.RETRY == null ? (callback.RETRY = opts.retry): callback.RETRY--;
+
+      if(callback.RETRY == null) {
+        callback.RETRY = options.retry || opts.retry
+      } else {
+        callback.RETRY--;
+      }
+
       callback.requestError = requestError;
       _timers[rid] = setTimeout(requestError, opts.timeout);
       _callbacks[rid] = callback;
+
     }
 
     if(ss._conn == null) {
-      if(!pushQueue(args)) {
-        return;
-      }
+      if(!pushQueue(args, rid)) return;
       return ss.connect(rid);
     }
 
     if(ss.isConnecting()) {
-      pushQueue(args);
+      pushQueue(args, rid);
       return;
     }
 
@@ -487,37 +557,32 @@
       return write();
 
     case Socket.CONNECTING:
-      pushQueue(args);
+      pushQueue(args, rid);
       return;
 
     case Socket.CLOSING:
-      if(!pushQueue(args)) {
-        return;
-      }
-      ss._index++;
-      return ss.connect(rid);
+      if(!pushQueue(args, rid)) return;
+      return ss._index++, ss.connect(rid);
 
     case Socket.CLOSED:
-      if(!pushQueue(args)) {
-        return;
-      }
-      ss._index++;
-      return ss.connect(rid);
+      if(!pushQueue(args, rid)) return;
+      return ss._index++, ss.connect(rid);
 
     default:
       mes = 'Unexpected readyState: ' + ss._conn.readyState;
-      requestError(mes, false);
-      return;
+      return requestError(mes, false);
 
     }
 
-    function pushQueue() {
-      if(_waits.length < max_wait) {
-        return _waits.push(args), true;
-      }
-      mes = 'Too many wait more than ' + max_wait;
-      requestError(mes, false);
+    function pushQueue(args, rid) {
+      _reset(rid);
+      if(_waits.length < max_wait) return _waits.push(args), true;
+      requestError('Too many wait more than ' + max_wait, false);
       return false;
+    }
+
+    function stringMessage(mess) {
+      return is('string', mess) ? mess: JSON.stringify(mess);
     }
 
     function write() {
@@ -525,35 +590,40 @@
       var conv = (opts.converter || Converter);
       var rurl, mess;
 
-      if(typeof args[0] == 'string' && args[1] == callback) {
+      if(is('string', args[0]) && args[1] == callback) {
         // send single raw string
         mess = args[0];
       } else {
-        // multiple arguments
+        // Multiple arguments
         mess = conv.apply(ss, [rid].concat(args));
       }
 
-      if(ss._conn.send) {
+      var so = ss._conn;
+      if(so.send) {
+
         // type: WebSocket
-        ss._conn.send(typeof mess == 'string' ? mess: JSON.stringify(mess));
+        so.send(stringMessage(mess));
+
       } else {
+
         // type: EventSource
         (browsing ? xmlPost: nodePost)(mess);
+
       }
 
     }
 
     function nodePost(mess) {
 
-      var host = ss._host, body = mess[1];
+      var host = ss._host;
+      var body = Array.isArray(mess) ? mess[1]: mess;
+      var url = body.url, headers = body.headers || {};
       var prtc = host.indexOf('https') === 0 ? 'https': 'http';
 
-      var headers = body.headers || {};
       headers['Content-Type'] = 'application/json';
-
       var options = {
         hostname: host.replace(prtc + '://', ''),
-        path: body.url,
+        path: url,
         method: 'POST',
         headers: headers,
         rejectUnauthorized: false
@@ -561,57 +631,77 @@
 
       //      console.log('MESSAGE!', mess, options);
 
+      // DON'T SEND URL and HEADERS, and DON'T FORGET "REVERT"!!
       delete body.url, delete body.headers;
       var r = require(prtc).request(options, function(res) {
 
-        // debuglog
-        //        console.log('nodePost returns for: ', options.path);
-        //        console.log(res.statusCode, body);
-        //
-        //        res.on('data', function(buf) {
-        //          console.log('nodePost.data: ' + buf.toString());
-        //        });
-        //        res.on('end', function() {
-        //          console.log('nodePost.end.');
-        //        });
+        // TODO check ok
+        if(!_cbOpcd) return;
 
-        // TODO
-        // res.statusCode == 200 ? dfd.resolve(): dfd.reject();
+        var t = '';
+        res.on('data', function(buf) {
+          t += buf.toString();
+        }).on('end', function() {
+          response(res.statusCode == 200, t, r);
+        });
 
       });
 
-      r.on('error', ss.onerror.bind(ss));
-      r.write(JSON.stringify(mess));
+      r.on('error', requestError);
+      r.write(stringMessage(mess));
       r.end();
+
+      body.url = url;
+      body.headers = headers;
 
     }
 
     function xmlPost(mess) {
 
-      var k, xhr = new XMLHttpRequest(), body = mess[1];
+      var k, xhr = new XMLHttpRequest();
+      var body = Array.isArray(mess) ? mess[1]: mess;
+      var url = body.url, headers = body.headers || {};
       xhr.open('POST', body.url, true);
 
-      var headers = body.headers || {};
       headers['Content-Type'] = 'application/json';
-
       xhr.withCredentials = true;
-      for(k in headers)
-        xhr.setRequestHeader(k, heaaders[k]);
+      for(k in headers) {
+        xhr.setRequestHeader(k, headers[k]);
+      }
 
       xhr.addEventListener('readystatechange', function() {
-        //        console.log('xmlPost returns.', xhr.status);
-        switch(xhr.readyState) {
-        case 4:
-          // TODO
-          // xhr.status == 200 ? dfd.resolve(): dfd.reject();
-        }
+        if(xhr.readyState != 4 || !_cbOpcd) return;
+        response(xhr.status == 200, xhr.responseText, xhr.response);
       });
 
-      xhr.addEventListener('error', onerror);
-      xhr.addEventListener('abort', onerror);
+      xhr.addEventListener('error', requestError);
+      xhr.addEventListener('abort', requestError);
 
+      // DON'T SEND URL and HEADERS, and DON'T FORGET "REVERT"!!
       delete body.url, delete body.headers;
-      xhr.send(JSON.stringify(mess));
+      xhr.send(stringMessage(mess));
+
+      body.url = url;
+      body.headers = headers;
+
+    }
+
+    function response(ok, xhr_t, xhr_x) {
+
+      var callback = _callbacks[rid] || Function();
+      if(ok) {
+
+        _reset(rid);
+        callback({
+          ok: 1,
+          message: xhr_t || xhr_x
+        });
+
+        // Callback immediately occurs on "opcodeCallback" response.
+        return;
+      }
+
+      requestError(xhr_t || xhr_x, false);
 
     }
 
@@ -626,21 +716,18 @@
       logger.error(mes);
       logger.error(ss._actors[ss._index]);
 
-      if(retry === false) {
-        callback(new Error(mes));
-        return;
-      }
+      e = new Error(mes);
+      if(callback.RETRY === false) return callback(e);
 
       if(ss.isConnecting()) {
-
         // One more retry => maybe queuing
         setTimeout(function() {
           ss.send.apply(ss, args);
         }, 80);
         return;
-
       }
 
+      var so = ss._conn;
       if(callback.RETRY > 0) {
 
         // Open, but not reachable for the network reason.
@@ -648,42 +735,76 @@
         logger.log('[StableSocket] request error occurs. readyState: '
           + ss.readyState() + ', retry remains: ' + callback.RETRY);
 
+        // Don't forget remove listeners and close socket.
+        // Old socket no longer be used.
+        ss.removeListeners(so), !so || so.close();
+
+        // Set reconnect condition and re-open new socket.
         ss.onLine = false, ss._conn = null, ss._index++;
         ss.send.apply(ss, args);
         return;
 
       }
 
-      callback(new Error(mes));
+      callback(e);
 
     }
 
   }
 
   function close() {
+
     var ss = this, logger = ss.logger;
-    var _conn = ss._conn;
+    var so = ss._conn;
+
     try {
-      ss._conn = null, _conn.close();
+      ss._conn = null;
+      so.close();
     } catch(e) {
       logger.log('[StableSocket] close error.', e);
     }
+
+  }
+
+  function removeListeners(so, evts, evts_map) {
+
+    var ss = this;
+    so = so || ss._conn || {};
+    evts_map = evts_map || so.EventHandler || {};
+    evts = Array.isArray(evts) ? evts: Object.keys(evts_map);
+
+    switch(true) {
+
+    case isFunction(so.removeEventListener):
+      evts.forEach(function(evt_ty) {
+        so.removeEventListener(evt_ty, evts_map[evt_ty] || Function());
+      });
+      break;
+
+    case isFunction(so.off):
+      evts.forEach(function(evt_ty) {
+        so.off(k, evts_map[evt_ty] || Function());
+      });
+      break;
+
+    }
+
+    // always overwrite ' on ... ' for avoid illegal handling.
+    evts.forEach(function(evt_ty) {
+      so['on' + evt_ty] = null;
+      delete evts_map[evt_ty];
+    });
+
   }
 
   function toSilentMode() {
 
     var ss = this;
-    if(ss._silent_timer) {
-      return;
-    }
+    if(ss._silent_timer) return;
 
     var term = ss._silent_term;
-    if(typeof term == 'function') {
-      term = term();
-    }
-    if(typeof term != 'number') {
-      return;
-    }
+    if(isFunction(term)) term = term();
+    if(!is('number', term)) return;
 
     // sign of mode change
     ss._silent_timer = setTimeout(function() {
@@ -747,8 +868,7 @@
       xhr = new XMLHttpRequest();
 
       xhr.onreadystatechange = function() {
-        if(xhr.readyState != xhr.DONE)
-          return;
+        if(xhr.readyState != xhr.DONE) return;
         clearNgTimer();
         (parseInt(String(xhr.status).charAt(0)) < 4 ? ok: ng)();
       };
@@ -810,7 +930,18 @@
    * @private
    */
   function _reset(rid) {
-    clearTimeout(_timers[rid]), delete _timers[rid], delete _callbacks[rid];
+    clearTimeout(_timers[rid]);
+    delete _timers[rid], delete _callbacks[rid];
+  }
+
+  function casting(x) {
+    return Array.prototype.slice.call(x);
+  }
+  function is(ty, x) {
+    return typeof x == ty;
+  }
+  function isFunction(x) {
+    return typeof x == 'function';
   }
 
 })(typeof window != 'undefined', typeof module != 'undefined');
